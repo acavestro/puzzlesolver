bd7b1239da26437c92cbe72a8784f5c0e99f82ae
Antonio Cavestro <antonio.cavestro@gmail.com>
2014-12-05 18:29:49 +0100

    Aggiungi implementazione PSTile dell'interfaccia Tile

---
+++ src/tile/PSTile.java
@@ -0,0 +1,50 @@
+package unipd.cs.p3.puzzlesolver.tile;
+
+class PSTile implements Tile {
+	private final String id;
+	private final String symbol;
+	private final String idUp;
+	private final String idDown;
+	private final String idLeft;
+	private final String idRight;
+
+	PSTile(String id, String symbol, String idUp, String idDown, String idLeft,
+		String idRight) {
+
+    this.id = id;
+    this.symbol = symbol;
+    this.idUp = idUp;
+    this.idDown = idDown;
+    this.idLeft = idLeft;
+    this.idRight = idRight;    
+  } 
+
+  public String getUp() {
+    return idUp;
+  }
+  public String getDown(){
+    return idDown;
+  }
+  public String getLeft(){
+    return idLeft;
+  }
+  public String getRight(){
+    return idRight;
+  }
+
+  public String getID(){
+    return id;
+  }
+
+  public String toString(){
+    return symbol;
+  }  
+
+  public boolean equals(Object o){
+    return o instanceof Tile && symbol.equals(o.toString);
+  }
+
+  public int hashCode(){
+    return id.hashCode();
+  }
+}
