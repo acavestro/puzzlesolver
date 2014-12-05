bea9cf1ea9e5b01aee877618ae77aa5f848c6259
Antonio Cavestro <antonio.cavestro@gmail.com>
2014-12-05 18:29:15 +0100

    Aggiungi interfaccia Tile

---
+++ src/tile/Tile.java
@@ -0,0 +1,12 @@
+package unipd.cs.p3.puzzlesolver.tile;
+
+public interface Tile {
+
+	public String getUp();
+	public String getDown();
+	public String getLeft();
+	public String getRight();
+
+	public String getID();
+
+}
