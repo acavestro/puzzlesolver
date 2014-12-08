703bee7d71671373b5a8fd4cd2d4267a4b66c9b9
Antonio Cavestro <antonio.cavestro@gmail.com>
2014-12-08 22:21:05 +0100

    Aggiungi interfaccia Puzzle

---
+++ src/puzzle/Puzzle.java
@@ -0,0 +1,11 @@
+package unipd.cs.p3.puzzlesolver.puzzle;
+
+import unipd.cs.p3.puzzlesolver.tile.Tile; 
+
+public interface Puzzle {
+
+	public int getRows();
+	public int getCols();
+
+	public Tile[][] toMatrix();
+}
