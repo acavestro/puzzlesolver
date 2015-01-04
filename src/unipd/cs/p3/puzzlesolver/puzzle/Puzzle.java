34f1fb283170ff5ae362f19d24424b78f3f3a7de
Antonio Cavestro <antonio.cavestro@gmail.com>
2015-01-04 15:23:55 +0100

    Fai refactoring classe Puzzle.

--- src/unipd/cs/p3/puzzlesolver/puzzle/Puzzle.java
+++ src/unipd/cs/p3/puzzlesolver/puzzle/Puzzle.java
@@ -1,12 +1,13 @@
 package unipd.cs.p3.puzzlesolver.puzzle;
 
-import unipd.cs.p3.puzzlesolver.tile.Tile; 
-
 public interface Puzzle {
 
-	public int getRows();
-	public int getCols();
-	public Object toLine();
-	public String toMatrix();
+  public int getRows();
+
+  public int getCols();
+
+  public Object toLine();
+
+  public String toMatrix();
 
 }
