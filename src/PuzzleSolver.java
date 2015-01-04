09ec913b436fc2b0e6d9cdc74b2310a046dec76c
Antonio Cavestro <antonio.cavestro@gmail.com>
2015-01-04 15:23:06 +0100

    Fai refactoring classe PuzzleSolver.

--- src/PuzzleSolver.java
+++ src/PuzzleSolver.java
@@ -1,75 +1,74 @@
-import unipd.cs.p3.puzzlesolver.tile.*;
-import unipd.cs.p3.puzzlesolver.puzzle.*;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.nio.charset.Charset;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Paths;
-import java.nio.file.Path;
-import java.nio.file.Files;
 import java.io.BufferedWriter;
 import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.HashMap;
+
+import unipd.cs.p3.puzzlesolver.puzzle.Puzzle;
+import unipd.cs.p3.puzzlesolver.puzzle.PuzzleBuilder;
+import unipd.cs.p3.puzzlesolver.puzzle.UnsolvablePuzzleException;
+import unipd.cs.p3.puzzlesolver.tile.IrregularTileLineException;
+import unipd.cs.p3.puzzlesolver.tile.Tile;
+import unipd.cs.p3.puzzlesolver.tile.TileParser;
 
 public class PuzzleSolver {
 
-	public static void main(String[] args) {
+  public static void main(String[] args) {
 
-    if(args.length != 2) {
+    if (args.length != 2) {
       System.out.println("Usage: PuzzleSolver INPUT_FILE OUTPUT_FILE");
       return;
     }
 
-    TileParser tp = new TileParser(args[0]);
+    final TileParser tp = new TileParser(args[0]);
     HashMap<String, Tile> m;
 
     try {
 
-        m = tp.getTiles();
+      m = tp.getTiles();
 
-    } catch(IrregularTileLineException itle) {
+    } catch (final IrregularTileLineException itle) {
 
-      System.out.println("ERROR: Some tiles are in a irregular format. " +
-                          "Check input file.\nAborting..");
+      System.out.println("ERROR: Some tiles are in a irregular format. "
+          + "Check input file.\nAborting..");
       return;
 
     }
 
-    PuzzleBuilder pb = new PuzzleBuilder(m);
+    final PuzzleBuilder pb = new PuzzleBuilder(m);
     Puzzle out;
 
     try {
 
       out = pb.solvePuzzle();
 
-    } catch(UnsolvablePuzzleException upe) {
+    } catch (final UnsolvablePuzzleException upe) {
 
-      System.out.println("ERROR: Missing tiles or wrong coordinates. " +
-                        "Unsolvable puzzle. Check input file.\nAborting... ");
+      System.out.println("ERROR: Missing tiles or wrong coordinates. "
+          + "Unsolvable puzzle. Check input file.\nAborting... ");
       return;
 
     }
 
-    String solution = out.toLine() + "\n" +
-                      "\n" +
-                      out.toMatrix() +
-                      "\n" +
-                      out.getRows() + " " +
-                      out.getCols();
+    final String solution = out.toLine() + "\n" + "\n" + out.toMatrix() + "\n"
+        + out.getRows() + " " + out.getCols();
 
-    Path outputFile = Paths.get(args[1]);
-    Charset charset = StandardCharsets.UTF_8;
+    final Path outputFile = Paths.get(args[1]);
+    final Charset charset = StandardCharsets.UTF_8;
 
     try (BufferedWriter writer = Files.newBufferedWriter(outputFile, charset)) {
 
       writer.write(solution);
 
-    } catch (IOException e) {
+    } catch (final IOException e) {
 
       System.err.println(e);
 
     }
 
-
-	}
+  }
 
 }
