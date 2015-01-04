4b6dac18df2d268ca6710ee78a16bbbedb28bc10
Antonio Cavestro <antonio.cavestro@gmail.com>
2015-01-04 15:17:26 +0100

    Fai refactoring della classe PuzzleBuilder.

--- src/unipd/cs/p3/puzzlesolver/puzzle/PuzzleBuilder.java
+++ src/unipd/cs/p3/puzzlesolver/puzzle/PuzzleBuilder.java
@@ -1,33 +1,34 @@
 package unipd.cs.p3.puzzlesolver.puzzle;
 
-import unipd.cs.p3.puzzlesolver.tile.Tile; 
-import java.util.HashMap;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Iterator;
+
+import unipd.cs.p3.puzzlesolver.tile.Tile;
 
 public class PuzzleBuilder {
 
-  private HashMap<String, Tile> unsolvedTiles;
+  private final HashMap<String, Tile> unsolvedTiles;
 
   public PuzzleBuilder(HashMap<String, Tile> tiles) {
-    this.unsolvedTiles = tiles;
+    unsolvedTiles = tiles;
   }
 
   private Tile[] solveFirstColumn() throws UnsolvablePuzzleException {
 
-    ArrayList<Tile> firstColumn = new ArrayList<Tile>();
+    final ArrayList<Tile> firstColumn = new ArrayList<Tile>();
     // Search for the first item in top left corner
-    Iterator i = unsolvedTiles.keySet().iterator();
+    final Iterator<String> i = unsolvedTiles.keySet().iterator();
     boolean found = false;
     String currentKey = null;
     Tile currentTile = null;
-    while(i.hasNext() && !found) {
+    while (i.hasNext() && !found) {
 
       currentKey = i.next().toString();
       currentTile = unsolvedTiles.get(currentKey);
 
-      if (currentTile.getLeft().equals("VUOTO") && 
-        currentTile.getUp().equals("VUOTO")) {
+      if (currentTile.getLeft().equals("VUOTO")
+          && currentTile.getUp().equals("VUOTO")) {
 
         firstColumn.add(currentTile);
         unsolvedTiles.remove(currentKey);
@@ -44,7 +45,7 @@ public class PuzzleBuilder {
 
     }
 
-    // Search the others until the end of line 
+    // Search the others until the end of line
     Tile nextTile = null;
     currentKey = firstColumn.get(0).getDown();
 
@@ -55,7 +56,7 @@ public class PuzzleBuilder {
       unsolvedTiles.remove(currentKey);
       currentKey = nextTile.getDown();
 
-    } 
+    }
 
     return firstColumn.toArray(new Tile[firstColumn.size()]);
 
@@ -63,7 +64,7 @@ public class PuzzleBuilder {
 
   private Tile[] solveRow(Tile start) throws UnsolvablePuzzleException {
 
-    ArrayList<Tile> row = new ArrayList<Tile>();
+    final ArrayList<Tile> row = new ArrayList<Tile>();
     row.add(start);
 
     Tile nextTile = null;
@@ -90,15 +91,15 @@ public class PuzzleBuilder {
   }
 
   public Puzzle solvePuzzle() throws UnsolvablePuzzleException {
-    Tile[] firstColumn = solveFirstColumn();
-    //firstColumn has at least one element, for sure. Otherwise, 
+    final Tile[] firstColumn = solveFirstColumn();
+    // firstColumn has at least one element, for sure. Otherwise,
     // UnsolvablePuzzleException would be thrown by solveFirstColumn().
-    Tile[] firstRow = solveRow(firstColumn[0]);
+    final Tile[] firstRow = solveRow(firstColumn[0]);
 
-    Tile[][] solution = new Tile[firstColumn.length][firstRow.length];
+    final Tile[][] solution = new Tile[firstColumn.length][firstRow.length];
     solution[0] = firstRow;
 
-    for (int i = 1; i < firstColumn.length; i++){
+    for (int i = 1; i < firstColumn.length; i++) {
 
       solution[i] = solveRow(firstColumn[i]);
 
