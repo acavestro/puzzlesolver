3a54479cc8c2d13e1004bb3d01808ba141eb96f3
Antonio Cavestro <antonio.cavestro@gmail.com>
2015-01-08 16:37:28 +0100

    Aggiungi test d'integrazione su normale funzionamento di PuzzleSolver.
    Contestualmente, aggiungi a gitignore file relativi al test.
    Related to: Segnalazione #6 su Redmine.

---
+++ tests/PuzzleSolverTest.java
@@ -0,0 +1,48 @@
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import org.junit.Test;
+
+public class PuzzleSolverTest {
+
+  @Test
+  public void puzzleSolverShouldWork() {
+
+    final String outName = "./puzzlesolvershouldwork.txt";
+    final String inputName = "./samples/input_100x100.txt";
+    final String solutionName = "./samples/solution_100x100.txt";
+
+    // Cancello file se presente
+    final File outFile = new File(outName);
+
+    if (outFile.exists()) {
+      if (outFile.delete()) {
+        fail("File esistente e fallita cancellazione");
+      }
+    }
+
+    // Invoco main
+
+    PuzzleSolver.main(new String[] { inputName, outName });
+
+    // Controllo il risultato.
+
+    final Path outPath = Paths.get(outName);
+    final Path solutionPath = Paths.get(solutionName);
+
+    try {
+      assertEquals(Files.readAllLines(outPath),
+          Files.readAllLines(solutionPath));
+    } catch (final IOException e) {
+      fail(e.getStackTrace().toString());
+    }
+
+  }
+
+}
